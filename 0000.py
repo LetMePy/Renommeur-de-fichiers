8562e287ee75271b51898618690ffbf17cc7ae52
LetMePy <skander.d.soltane@gmail.com>
2019-06-05 08:05:33 +0100

    Adding arguments [-h] [-l LENGTH] [-v] path

--- 0000.py
+++ 0000.py
@@ -1,32 +1,75 @@
+import argparse
 import os
 
-length = 4
 
-# The file's name respect the format. 0042.png
+# --- Functions ---
+
+
+# The file's name respect the format. '0042.png'
 is_valid = lambda s: len(s) == length and s.isdigit()
 
-# Return the name without extension. foo.png -> foo
+# Return the name without extension. 'foo.png' -> 'foo'
 clean = lambda s: s[:s.rfind('.')] if '.' in s else s
 
-# Given an integer k return the equivalent in string valid format. 42 -> 0042
-make_name = lambda k: '0' * (length - len(str(k))) + str(k)
+# '0042' -> '0043'
+inc = lambda s: '0' * (length - len(str(int(s) + 1))) + str(int(s) + 1)
 
-valid_names = {name for name in sorted(os.listdir()) if is_valid(clean(name))}
-not_valid_names = set(os.listdir()) - valid_names
 
-previous_name = '0' * length
-valid_names.remove(__file__)
-for name in sorted(valid_names):
-	if clean(name) != make_name(int(clean(previous_name)) + 1):
-		not_valid_names.add(name)
-		valid_names.remove(name)
+# --- Argument Management ---
+
+
+parser = argparse.ArgumentParser()
+# The path where the files are
+parser.add_argument('path', help='The path where the files are')
+# The length
+parser.add_argument('-l', '--length', type=int,
+					help='Specify the length of the new names. default = 4')
+# Activate the warning message
+parser.add_argument('-v', '--verbose', action='store_true',
+					help='Display a confirmation message')
+args = parser.parse_args()
+
+path = args.path
+if not os.path.exists(path):
+	raise FileNotFoundError
+	exit(1)
+
+length = args.length if args.length else 4
+verbose = args.verbose
+
+
+# --- Script ---
+
+
+names = sorted(os.listdir(path))
+valid_names = []
+invalid_names = []
+if path == '.':
+	names.remove(__file__)
+
+# Charging the list of invalid names
+last_name = '0' * length
+for name in names:
+	if is_valid(clean(name)) and clean(name) == inc(clean(last_name)):
+		valid_names.append(name)
+		last_name = clean(name)
 	else:
-		previous_name = clean(name)
+		invalid_names.append(name)
 
-last_valid_name = previous_name
-for name in list(not_valid_names):
-	new_name = make_name(int(last_valid_name) + 1) + name[name.rfind("."):]
-	last_valid_name = clean(new_name)
-	input(f'Renaming <{name}> into <{new_name}>. Press Enter..')
-	os.rename(name, new_name)
-	print('Done ✔.')
+# Renaming the invalid names
+for name in invalid_names:
+	
+	# The new valid name is the incrementation of the last one
+	new_name = inc(last_name)
+	last_name = new_name
+
+	# Adding the extension
+	final_name = new_name + name[name.rfind('.'):]
+
+	# The user confirm the change
+	if args.verbose:
+		input(f'Renaming <{name}> into <{final_name}>... [Enter/CTRL+C]')
+		print('✔ Done ✔.')
+
+	os.rename(os.path.join(path, name), os.path.join(path, final_name))
+	
