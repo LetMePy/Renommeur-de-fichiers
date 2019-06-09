1367e23df16488617142e419c5113aa42e894b67
LetMePy <skander.d.soltane@gmail.com>
2019-06-09 19:02:23 +0100

    Support for 9 -> 10 format (without length)

--- manage.py
+++ manage.py
@@ -5,8 +5,8 @@ import os
 # --- Functions ---
 
 
-# The file's name respect the format. '0042.png'
-is_valid = lambda s: len(s) == length and s.isdigit()
+# Verify if the file's name respect the format. '0042.png' if length is 4
+is_valid = lambda s: s.isdigit() and (len(s) == length if length else s[0] != '0')
 
 # Return the name without extension. 'foo.png' -> 'foo'
 clean = lambda s: s[:s.rfind('.')] if '.' in s else s
@@ -23,7 +23,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('path', help='The path where the files are')
 # The length
 parser.add_argument('-l', '--length', type=int,
-					help='Specify the length of the new names. default = 4')
+					help='the length of the new names. default = 4')
 # Activate the warning message
 parser.add_argument('-v', '--verbose', action='store_true',
 					help='Display a confirmation message')
@@ -34,13 +34,13 @@ if not os.path.exists(path):
 	raise FileNotFoundError
 	exit(1)
 
-length = args.length if args.length else 4
+length = args.length if args.length else 0
 verbose = args.verbose
 
 
 # --- Script ---
 
-# Loading the list of names to verify
+# Loading the list of the names to verify
 names = []
 for name in sorted(os.listdir(path)):
 	if os.path.isfile(os.path.join(path, name)):
@@ -51,7 +51,7 @@ if path == '.':
 # Loading the list of invalid names
 valid_names = []
 invalid_names = []
-last_name = '0' * length
+last_name = '0' * length if length else '0'
 for name in names:
 	if is_valid(clean(name)) and clean(name) == inc(clean(last_name)):
 		valid_names.append(name)
